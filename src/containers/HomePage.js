a53d538b6a5519249dc88a3ebbbac89f16260cfb
cwza <cwz0205a@gmail.com>
2016-12-27 13:00:38 +0800

    add Post Component

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import postsActions from '../actions/postsActions'
 import * as postsSelectors from '../selectors/postsSelectors'
-// import Post from '../components/Post'
+import Post from '../components/Post'
 
 let queryStr = 'q=userId:(1, 2)&sort=createTime&order=desc&limit=5';
 // let queryParams = {userIds: [1], contentContains: '#cwz', sort: 'createTime', order: 'desc', limit: 5};
@@ -17,7 +17,7 @@ class HomePage extends Component {
       <div>
         <h1>I am Home Page.</h1>
         <button onClick={this.props.fetchNewPostsStart}>Load New</button>
-        {posts.map((post, i) => <h1 key={i}>{JSON.stringify(post, null, 2)}</h1>)}
+        {posts.map((post, i) => <Post key={i} i={i} post={post} />)}
         <button onClick={this.props.fetchOldPostsStart}>Load Old</button>
       </div>
     )
