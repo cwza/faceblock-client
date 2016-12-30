8e04091a4535d0a882fc14ccf82cd34e271f9d87
cwza <cwz0205a@gmail.com>
2016-12-30 14:33:30 +0800

    fix test bug

--- src/sagas/postsSagas.spec.js
+++ src/sagas/postsSagas.spec.js
@@ -7,17 +7,18 @@ import { getPostsForHomePageByTime } from '../selectors/postsSelectors'
 import { posts } from '../mockDatas/data'
 const postsSagas = require('./postsSagas').private;
 
-describe('#fetchPosts()', () => {
+describe('#callPostsApi() fetchPosts', () => {
   it('should call postsService.fetchPosts if success should dispatch fetchPostsSuccess otherwise dispatch fetchPostsError', () => {
     let queryStr = 'q=userId:(1)&sort=createTime&order=desc&limit=5';
     let response = {response: {}};
     let error = {error: {}};
-    let iter = postsSagas.callPostsApi('fetchPosts', queryStr);
+    let apiName = 'fetchPosts';
+    let iter = postsSagas.callPostsApi(apiName, queryStr);
     expect(iter.next().value).to.deep.equal(
-      call(postsService.fetchPosts, queryStr)
+      call(postsService[apiName], queryStr)
     );
     expect(iter.next(response).value).to.deep.equal(
-      put(postsActions.fetchPostsSuccess(response))
+      put(postsActions[apiName + 'Success']({response}))
     );
     expect(iter.throw(error).value).to.deep.equal(
       put(otherActions.setError({error}))
