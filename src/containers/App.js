8dc3f376528405d34dc3952a821eba5df24bab70
cwza <cwz0205a@gmail.com>
2017-01-05 19:57:50 +0800

    fetch selfUser when login

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,14 +5,21 @@ import { getError } from '../selectors/utilsSelectors'
 import Error from '../components/Error'
 import authenticationActions from '../actions/authenticationActions'
 import { getAuthentication } from '../selectors/utilsSelectors'
-import { getSelfId } from '../selectors/usersSelectors'
+import { getSelfId, getSelfUser } from '../selectors/usersSelectors'
 import SideBar from '../components/SideBar'
+import usersActions from '../actions/usersActions'
+import { isEmpty } from 'lodash'
 
 class App extends Component {
+  componentDidMount() {
+    let { selfUser, selfId, fetchUserStart } = this.props;
+    if(isEmpty(selfUser) && selfId)
+      fetchUserStart(selfId);
+  }
   componentDidUpdate() {
-    let { error } = this.props;
+    let { error, handleLogout } = this.props;
     if(error && error.name === 'AUTHENTICATION_ERROR') {
-      this.handleLogout();
+      handleLogout();
     }
   }
   handleLogout = () => {
@@ -41,10 +48,12 @@ const mapStateToProps = (state) => {
     error: getError(state),
     faceblockToken: getAuthentication(state).item.faceblockToken,
     selfId: getSelfId(state),
+    selfUser: getSelfUser(state)
   }
 }
 
 export default connect(mapStateToProps, {
   logout: authenticationActions.logout,
   routerPush: routerActions.push,
+  fetchUserStart: usersActions.fetchUserStart,
 })(App);
