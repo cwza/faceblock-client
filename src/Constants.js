739d28156d5de374aebe2c3625c85ddb074a7df2
cwza <cwz0205a@gmail.com>
2017-01-17 21:58:22 +0800

    add production host

--- src/Constants.js
+++ src/Constants.js
@@ -1,6 +1,6 @@
 const POSTS_COUNT_PER_PAGE = 10;
 const DATETIME_FIELDS = ['createTime', 'updateTime'];
-const HOST = 'http://127.0.0.1:3000'
+const HOST = process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:3000' : 'https://cwza.github.io'
 const ROOT_PATH = '/faceblock-client'
 
 export { POSTS_COUNT_PER_PAGE, DATETIME_FIELDS, HOST, ROOT_PATH };
